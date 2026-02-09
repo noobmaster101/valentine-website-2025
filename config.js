1567ac30c273b3e95d037ba531108e83b9b85fe3
noobmaster101 <50867761+noobmaster101@users.noreply.github.com>
2026-02-09 23:46:58 +0530

    config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine Bestfriend for 2 years? 🌹", // The big question!
+            text: "Will you be my Valentine Bestfriend for 2 years? 🫡🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge!",
+        message: "Now come get your gift, a big warm hug 🫂!",
         emojis: "🎁💖🤗💝❤️💕"  // These will bounce around
     },
 
